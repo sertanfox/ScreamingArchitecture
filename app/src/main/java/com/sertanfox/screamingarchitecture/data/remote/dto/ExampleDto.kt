b426ed06dd63b65ee3cd5712406d28adf3d0ebba
sertanfox <39944474+sertanfox@users.noreply.github.com>
2023-11-25 19:27:41 +0300

    Example codes deleted.

--- app/src/main/java/com/sertanfox/screamingarchitecture/data/remote/dto/ExampleDto.kt
+++
@@ -1,6 +0,0 @@
-package com.sertanfox.screamingarchitecture.data.remote.dto
-
-data class ExampleDto(
-    val name:String? = null,
-    val surname:String? = null
-)
--- app/src/main/java/com/sertanfox/screamingarchitecture/domain/repository/ExampleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.sertanfox.screamingarchitecture.domain.repository
-
-import com.sertanfox.screamingarchitecture.data.remote.dto.ExampleDto
-
-interface ExampleRepository {
-    suspend fun login(email:String,password:String):ExampleDto
-}
